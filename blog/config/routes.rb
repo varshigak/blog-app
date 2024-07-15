7994bd7f2e39693e38ad24145451b00f35de1bae
varshigak <varshigak@gmail.com>
2024-07-14 22:26:07 -0400

    Implement ability to create, read, update and delete articles

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -8,5 +7,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "articles#index"
+  resources :articles
 end
